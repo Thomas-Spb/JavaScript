919ae661f989644bde23645ac2d57f33aa3c3c08
Thomas-Spb <thomas-spb@list.ru>
2024-02-13 16:21:15 +0300

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+'use sctrict';
+
+const carBrand = 'Vaz';
+console.log('carBrand: ', carBrand);
+let carsQuantity = 5;
+const category = 'Car';
+let carPrice = 500;
+
+console.log('totalPrice:', carsQuantity * carPrice);
