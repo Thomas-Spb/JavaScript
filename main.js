5c58a87ac25995201dd244bb66d01dcc9e4aee22
Thomas-Spb <thomas-spb@list.ru>
2024-02-13 16:25:35 +0300

    add 2 item

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 'use sctrict';
 
-const carBrand = 'Vaz';
-console.log('carBrand: ', carBrand);
-let carsQuantity = 5;
-const category = 'Car';
-let carPrice = 500;
+{
+  const carBrand = 'Vaz';
+  let carsQuantity = 5;
+  const category = 'Car';
+  let carPrice = 500;
 
-console.log('totalPrice:', carsQuantity * carPrice);
+  console.log('carBrand: ', carBrand);
+  console.log('totalPrice:', carsQuantity * carPrice);
+}
+
+{
+  const item = 'smart проектор philips';
+  const count = 15;
+  const category = 'видео техника';
+  const price = 1200;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
