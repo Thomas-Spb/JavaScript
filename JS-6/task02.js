a108c4500dd95d129c763ef9987c2d5f5cbaceed
Thomas-Spb <thomas-spb@list.ru>
2024-02-14 19:07:37 +0300

    JS for-while

---
+++ JS-6/task02.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+  const isPrime = num => {
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  };
+
+  console.log(isPrime(152));
+}
